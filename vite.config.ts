8250be9262bdf9e90e2b6b8def29a951d74108c8
MinttDrazi <martinditz@gmail.com>
2026-08-16 22:12:02 +0200

    Fix: add trailing slash to base URL in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/<REPO>",
+  base: "/<REPO>/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
